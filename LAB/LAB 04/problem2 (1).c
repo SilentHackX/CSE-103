966cc207f78fb0c66e005f5357a04a0fb6d84650
Al Amin <80200600+SilentHackX@users.noreply.github.com>
2024-12-26 01:24:21 +0600

    Add files via upload

---
+++ LAB/LAB 04/problem2 (1).c
@@ -0,0 +1,28 @@
+/*In this problem, you will develop a program that can find how many times an integer
+number can be divided by Two (2).
+Sample Input Sample Output
+32 				5
+61 				0 */
+
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    int Num, i = 0;
+   
+    printf("Enter a Number: ");
+    scanf("%d", &Num);
+    
+    while (Num % 2 == 0 && Num > 0) // Ensure Num is divisible by 2 and positive
+    {
+        Num /= 2; // Divide by 2
+        i++;      // Increment the counter for each division by 2
+    }
+
+    printf("The number of times the number is divisible by 2: %d" , i);
+
+    return 0;
+}
