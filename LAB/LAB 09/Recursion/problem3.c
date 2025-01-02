9452ddefac481d15480dbcc1e4deaf83bdff4aee
Al Amin <80200600+SilentHackX@users.noreply.github.com>
2025-01-02 20:53:31 +0600

    Update problem3.c

--- LAB/LAB 09/Recursion/problem3.c
+++ LAB/LAB 09/Recursion/problem3.c
@@ -1,3 +1,10 @@
+/*Design a structure named ”Car” to store details like car ID, model, and rental rate per day. Write a
+C program to input data for three cars, calculate the total rental cost for a specified number of
+days, and display the results.*/
+
+
+
+
 #include<stdio.h>
 
 typedef struct
